249f346faa388af795690489092a24cff33a7a44
silvak <silvak.jeg@gmail.com>
2024-01-03 19:48:44 -0400

    add teacher's resolution

--- projects/react-prueba-tecnica/src/App.jsx
+++ projects/react-prueba-tecnica/src/App.jsx
@@ -1,4 +1,57 @@
 import { useEffect, useState } from "react";
+import "./App.css";
+
+const CAT_ENDPOINT_RANDOM_FACT = "https://catfact.ninja/fact";
+const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/:text?fontSize=:size&fontColor=:color`;
+const CAT_PREFIX_IMAGE_URL = `https://cataas.com`;
+
+export function App() {
+  const [fact, setFact] = useState();
+  const [word, setWord] = useState();
+  const [factError, setFactError] = useState();
+
+  // recuperar la cita al cargar la pagina
+  useEffect(() => {
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+      .then((response) => {
+        // si hay un error con la respuesta
+        if (!response.ok) throw new Error("Error fetching fact");
+        return response.json();
+      })
+      .then((data) => {
+        const { fact } = data;
+        setFact(fact);
+      })
+      .catch((err) => {
+        // tanto si hay un error con la respuesta
+        // como si hay un error con la petición
+        console.log(err);
+      });
+  }, []);
+
+  // recuperar el texto de imagen cada vez que tenemos una cita nueva
+  useEffect(() => {
+    if (!fact) return;
+    let firstWord = fact.split(" ", 3).join(" ");
+    setWord(firstWord);
+  }, [fact]);
+
+  return (
+    <main>
+      <h1>App gatos</h1>
+      {fact && <p>{fact}</p>}
+      {word && (
+        <img
+          src={`https://cataas.com/cat/says/${word}?fontSize=50&fontColor=red`}
+          alt={`Image extracted using the first three words for ${fact}`}
+        />
+      )}
+    </main>
+  );
+}
+
+/*
+import { useEffect, useState } from "react";
 
 export function App() {
   const [sentence, setSentence] = useState("");
@@ -39,3 +92,5 @@ export function App() {
     </main>
   );
 }
+
+*/
