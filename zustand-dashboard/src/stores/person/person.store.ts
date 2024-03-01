40aa4edd256e0ba6df3d564225d4b8121c1c6ee5
silvak <silvak.jeg@gmail.com>
2024-03-01 12:24:49 -0400

    add custom storage - persist

--- zustand-dashboard/src/stores/person/person.store.ts
+++ zustand-dashboard/src/stores/person/person.store.ts
@@ -1,9 +1,40 @@
-//
+//custom storage pattern
+import { type StateCreator, create } from "zustand";
+import { StateStorage, createJSONStorage, persist } from "zustand/middleware";
 
 interface PersonState {
   firstName: string;
   lastName: string;
+}
 
+interface Actions {
   setFirstName: (value: string) => void;
   setLastName: (value: string) => void;
 }
+
+const storeAPI: StateCreator<PersonState & Actions> = (set) => ({
+  firstName: "",
+  lastName: "",
+  setFirstName: (value: string) => set((state) => ({ firstName: value })),
+  setLastName: (value: string) => set((state) => ({ lastName: value })),
+});
+
+const sessinStorage: StateStorage = {
+  getItem: function (name: string): string | Promise<string | null> | null {
+    console.log("getItem", { name });
+    return null;
+  },
+  setItem: function (name: string, value: string): void | Promise<void> {
+    console.log("setItem", { name, value });
+  },
+  removeItem: function (name: string): void | Promise<void> {
+    console.log("removeItem", { name });
+  },
+};
+
+export const usePersonStore = create<PersonState & Actions>()(
+  persist(storeAPI, {
+    name: "person-storage",
+    storage: createJSONStorage(() => sessinStorage),
+  })
+);
