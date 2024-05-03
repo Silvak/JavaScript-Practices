1930726ae13b8d9df6b05a08c480f7e169b6653b
silvak <silvak.jeg@gmail.com>
2024-05-03 03:25:33 -0400

    add

--- zustand-dashboard/src/stores/tasks/task.store.ts
+++ zustand-dashboard/src/stores/tasks/task.store.ts
@@ -17,7 +17,6 @@ interface TaskState {
   changeTaskStatus: (taskId: string, status: TaskStatus) => void;
   onTaskDrop: (status: TaskStatus) => void;
 }
-//
 
 const storeApi: StateCreator<TaskState, [["zustand/immer", never]]> = (
   set,
