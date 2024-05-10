ab5863aab41caff578059f83076c19bda2005996
silvak <silvak.jeg@gmail.com>
2024-05-09 21:23:30 -0400

    add other slice into boundstore

--- zustand-dashboard/src/stores/wedding/index.ts
+++ zustand-dashboard/src/stores/wedding/index.ts
@@ -1,12 +1,15 @@
 // crear el store
 import { create } from "zustand";
 import { createPeronSlice, PersonSlice } from "./person.slice";
+import { createGuestCount, GuestSlice } from "./guest.slice";
 import { devtools } from "zustand/middleware";
 
-type ShareState = PersonSlice;
+type ShareState = PersonSlice & GuestSlice;
+//interface IStore extends PersonSlice, GuestSlice {}
 
 export const useWeddingBoundStore = create<ShareState>()(
   devtools((...a) => ({
     ...createPeronSlice(...a),
+    ...createGuestCount(...a),
   }))
 );
