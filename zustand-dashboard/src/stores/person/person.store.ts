93120a733b50ec9eb840d19b14ce63913dcf0e19
silvak <silvak.jeg@gmail.com>
2024-05-14 21:00:59 -0400

    add subscribers

--- zustand-dashboard/src/stores/person/person.store.ts
+++ zustand-dashboard/src/stores/person/person.store.ts
@@ -1,8 +1,9 @@
 //custom storage pattern
 import { type StateCreator, create } from "zustand";
 import { devtools, persist } from "zustand/middleware";
+import { useWeddingBoundStore } from "../wedding";
 //import { customSessionStorage } from "../storages/session.storage";
-import { firebaseStorage } from "../storages/firebase.storage";
+//import { firebaseStorage } from "../storages/firebase.storage";
 //import { logger } from "../middlewares/logger.middleware";
 
 interface PersonState {
@@ -38,3 +39,10 @@ export const usePersonStore = create<PersonState & Actions>()(
   )
   //)
 );
+
+usePersonStore.subscribe((nextState /*prevState*/) => {
+  const { firstName, lastName } = nextState;
+
+  useWeddingBoundStore.getState().setFirstName(firstName);
+  useWeddingBoundStore.getState().setLastName(lastName);
+});
