7dd4910de02d3d869f34068af68c2411208819b3
silvak <silvak.jeg@gmail.com>
2024-06-15 08:13:59 -0400

    add

--- zustand-dashboard/src/pages/dashboard/DashboardPage.tsx
+++ zustand-dashboard/src/pages/dashboard/DashboardPage.tsx
@@ -14,7 +14,6 @@ import {
   useTaskStore,
 } from "../../stores";
 
-
 //
 export const Dashboard = () => {
   const totalBears = useBearStore((state) => state.totalBears);
@@ -30,39 +29,39 @@ export const Dashboard = () => {
       <p>Información colectiva de varios stores de Zustand</p>
       <hr />
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4'>
         <WhiteCard centered>
-          <IoPawOutline size={50} className="text-indigo-600" />
+          <IoPawOutline size={50} className='text-indigo-600' />
           <h2>Osos</h2>
           <p>{totalBears()}</p>
         </WhiteCard>
 
         <WhiteCard centered>
-          <IoAccessibilityOutline size={50} className="text-indigo-600" />
+          <IoAccessibilityOutline size={50} className='text-indigo-600' />
           <h2>Persona</h2>
           <p>{firstName}</p>
         </WhiteCard>
 
         <WhiteCard centered>
-          <IoListOutline size={50} className="text-indigo-600" />
+          <IoListOutline size={50} className='text-indigo-600' />
           <h2>Tareas</h2>
           <p>{TaskCount}</p>
         </WhiteCard>
 
         <WhiteCard centered>
-          <IoHeartOutline size={50} className="text-indigo-600" />
+          <IoHeartOutline size={50} className='text-indigo-600' />
           <h2>Boda</h2>
           <p>Información</p>
         </WhiteCard>
 
         <WhiteCard centered>
-          <IoLockClosedOutline size={50} className="text-indigo-600" />
+          <IoLockClosedOutline size={50} className='text-indigo-600' />
           <h2>Auth</h2>
           <p>{userName}</p>
         </WhiteCard>
 
-        <WhiteCard centered className="col-span-3 overflow-hidden">
-          <IoInformationOutline size={50} className="text-indigo-600" />
+        <WhiteCard centered className='col-span-3 overflow-hidden'>
+          <IoInformationOutline size={50} className='text-indigo-600' />
           <h2>Auth</h2>
           <p>Información</p>
           <RequestInfo />
