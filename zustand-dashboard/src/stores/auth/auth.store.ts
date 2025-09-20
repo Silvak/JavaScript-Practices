7e6852f4dfcacd3e15649d43c6f72704fb83c320
silvak <silvak.jeg@gmail.com>
2025-09-20 01:15:46 -0400

    add

--- zustand-dashboard/src/stores/auth/auth.store.ts
+++ zustand-dashboard/src/stores/auth/auth.store.ts
@@ -12,7 +12,8 @@ export interface AuthState {
   checkAuthStatus: () => Promise<void>;
   logoutUser: () => void;
 }
-//
+
+
 const storeApi: StateCreator<AuthState> = (set) => ({
   status: "pending",
   token: undefined,
